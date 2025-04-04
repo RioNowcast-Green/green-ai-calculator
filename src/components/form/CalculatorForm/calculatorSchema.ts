30ee6578b20a08b423ac0516a2d6acae764cea87
Gabriel Breder <gabrielbertobreder@gmail.com>
2025-04-04 08:05:04 -0300

    feat: update calculator schema

--- src/components/form/CalculatorForm/calculatorSchema.ts
+++ src/components/form/CalculatorForm/calculatorSchema.ts
@@ -1,10 +1,24 @@
 import { z } from "zod";
 
 export const calculatorSchema = z.object({
-  energy: z.number(),
-  onSiteWUE: z.number(),
-  offSiteWUE: z.number(),
-  PUE: z.number(),
+  time: z
+    .string()
+    .transform((val) => Number(val))
+    .refine((val) => !isNaN(val), {
+      message: "Time must be a number",
+    }),
+  onSiteWUE: z
+    .string()
+    .transform((val) => Number(val))
+    .refine((val) => !isNaN(val), {
+      message: "onSiteWUE must be a number",
+    }),
+  PUE: z
+    .string()
+    .transform((val) => Number(val))
+    .refine((val) => !isNaN(val), {
+      message: "PUE must be a number",
+    }),
 });
 
 export type CalculatorSchemaType = z.infer<typeof calculatorSchema>;
