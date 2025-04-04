15bbb361638d7b6988aa3ecb500083d451665e18
Gabriel Breder <gabrielbertobreder@gmail.com>
2025-04-04 08:01:40 -0300

    feat: change header title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <header className="bg-green p-3 text-center">
-      <h1 className="text-2xl bold font-primary">Calculator</h1>
+      <h1 className="text-2xl bold font-primary">Calculadora</h1>
     </header>
   );
 };
