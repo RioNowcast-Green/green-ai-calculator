80f6ed2d004be6f67ab8985d412fda916d34b6bb
Gabriel Breder <gabrielbertobreder@gmail.com>
2025-04-04 08:04:39 -0300

    feat: add compartion ideas

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,19 @@ function App() {
   return (
     <>
       <Header />
-      <Form />
+
+      <div className="flex items-center justify-around my-20">
+        <Form />
+        <div className="flex flex-col gap-5">
+          <p>Comparar a energia com lâmpadas convencionais</p>
+          <p>Comparar a pegada de carbono com carros populares brasileiros</p>
+          <p>Comparar a pegada hídrica com garrafas d'água de 500ml</p>
+          <p>
+            Adicionar uma string para uso em publicações com o resumo dos
+            resultados
+          </p>
+        </div>
+      </div>
     </>
   );
 }
