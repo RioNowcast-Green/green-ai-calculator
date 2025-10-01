83b38fc37c77c93b6f8033adfe7d8950ff2b5d64
Gabriel <gabrielbertobreder@gmail.com>
2025-10-01 20:36:09 -0300

    feat: update article link

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -94,7 +94,7 @@ export const Home = () => {
                   {context.water_consumed.toFixed(2)} L of water in{" "}
                   {context.country}, as calculated with wAIter (beta version)
                   <a
-                    href="https://zenodo.org/records/17238927"
+                    href="https://zenodo.org/records/17247278"
                     className="text-link underline hover:text-link hover:underline"
                     target="_blank"
                     rel="noopener noreferrer"
